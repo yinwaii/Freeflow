e49d32b73cbbecedec197a9e9a3a10eabd6713b2
yinwai <58301225+yinwaii@users.noreply.github.com>
2024-03-15 08:21:03 +0800

    Update build-client.sh

--- build-client.sh
+++ build-client.sh
@@ -1,12 +1,23 @@
+cd libmempool/
+make
+make install
+cd ..
+
 cd libraries/libmlx4-1.2.1mlnx1/
+./autogen.sh
+./configure
 make
 make install
 
 cd ../libibverbs-1.2.1mlnx1/
+./autogen.sh
+./configure --prefix=/usr LIBS="-lmempool -lrt"
 make
 make install
 
 cd ../librdmacm-1.1.0mlnx/
+./autogen.sh
+./configure --prefix=/usr LIBS="-lmempool -lrt"
 make
 make install
 
